1c4f76b24cc19f8906d22e40235e800c5748cd93
bssayla <ouaicha47@gmail.com>
2023-03-13 16:26:25 +0100

    added

--- testing/MyFrame.java
+++ testing/MyFrame.java
@@ -2,36 +2,65 @@ package testing;
 
 
 
-
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
 
 
 
-class MyFrame {
+
+class MyFrame extends JFrame{
+    int nombreDesAttributes;
     
-        public static void main(String[] args) {
-            
-            JFrame frame = new JFrame("MyFrame");
-            JButton button = new JButton("Press me");
-            JLabel label = new JLabel("Hello World");
-            JLabel label2 = new JLabel("Hello World2");
-            JTextField text = new JTextField();
-            // frame.add(button);
-            
-            //setBounds(x, y, width, height)
-            label.setBounds(50, 50, 100, 30);
-            label2.setBounds(50, 100, 100, 30);
-            // button.setBounds(10, 50, 100, 30);
-            text.setBounds(110, 50, 200, 30);
-            frame.add(label);
-            frame.add(label2);
-            frame.add(text);
-
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(400, 400);
-            frame.setVisible(true);
-        }
+    public MyFrame(int nombreDesAttributes){
+        this.nombreDesAttributes = nombreDesAttributes;
+        initComponents();
     }
+    private void initComponents() {
+        for(int i = 0; i<nombreDesAttributes; i++){
+            JLabel attributeName = new JLabel("Nom d'Attribute n° "+i + " :");
+            JTextField nameTextField = new JTextField();
+            JLabel attributeType = new JLabel("Type d'Attribute n°"+i +" :");
+            JLabel attributeValue = new JLabel("Valeur d'Attribute n°"+i +" :");
+            JTextField valueTextField = new JTextField();
+            
+            GroupLayout layout = new GroupLayout(getContentPane());
+            getContentPane().setLayout(layout);
+            layout.setHorizontalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(attributeName)
+                        .addComponent(nameTextField, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+                        .addComponent(valueTextField)
+                    .addContainerGap(280, Short.MAX_VALUE))
+            );
+            layout.setVerticalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(attributeName)
+                    .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(nameTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(233, Short.MAX_VALUE))
+            );
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MyFrame f= new MyFrame(5);
+        f.setVisible(true);
+        f.setSize(400,400);
+    }
+
+
+
+}
