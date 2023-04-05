6b258f4e86ac0b015e1b92cbccdca98ce29a2aea
bssayla <ouaicha47@gmail.com>
2023-04-05 16:07:47 +0000

    Added

--- Interfaces/App.java
+++ Interfaces/App.java
@@ -2,14 +2,14 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.List;
 
 import javax.swing.JFrame;
 
-import Panels.Attributes;
 import Panels.CardPanels;
 import Panels.LesNomsDesClasses;
-import Panels.Methodes;
 import Panels.nombreDesClass;
+import Panels.Methodes;
 import Pieces.Footer;
 import Pieces.Header;
 import Pieces.MenuBar;
@@ -17,6 +17,7 @@ import Pieces.SideBar;
 
 public class App extends JFrame {
     int currentPage = 1;
+    int nombreDesClasses = -1;
  
     
     public App() {
@@ -45,13 +46,14 @@ public class App extends JFrame {
         myFooter.nextButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 if(currentPage == 1){
-                    toLesNomsDesClassesPages();
+                    nombreDesClasses = ((nombreDesClass)cardPanels.getComponents()[0]).getNombreDesClasses();
+                    dansLeNombreDesClassesPage(nombreDesClasses);
                      
                 }else if(currentPage == 2){
-                    ((CardLayout)cardPanels.getLayout()).show(cardPanels, "methodes");
+                    nombreDesClasses = ((LesNomsDesClasses)cardPanels.getComponents()[1]).getNombreDesClasses();
+                    dansLesNomsDesClassesPage(nombreDesClasses);
                 }
                 else if(currentPage == 3){
-                    ((CardLayout)cardPanels.getLayout()).show(cardPanels, "attributes");
                 }
 
 
@@ -70,19 +72,42 @@ public class App extends JFrame {
         setSize(1200, 800);
     }
 
-    public void toLesNomsDesClassesPages() {
+    public void dansLeNombreDesClassesPage(int nombreDesClasses) {
         //get the number of class from the first page
-        int nombreDeClass = ((nombreDesClass)cardPanels.getComponents()[0]).getNombreDeClass();
-        if(nombreDeClass == -1){
+        if(nombreDesClasses == -1){
             ((nombreDesClass)cardPanels.getComponents()[0]).getErrorsField().setText("Il faut donner un nombre puis clicker sur SUBMIT");
         }else{
-                         
+            ((LesNomsDesClasses)cardPanels.getComponents()[1]).setNombreDesClasses(nombreDesClasses);
             //print the number of class in component 2
+
             ((CardLayout)cardPanels.getLayout()).show(cardPanels, "2");
-            ((LesNomsDesClasses)cardPanels.getComponents()[1]).getErrorsField().setText("Le nombre est "+nombreDeClass);
+            ((LesNomsDesClasses)cardPanels.getComponents()[1]).getErrorsField().setText("Le nombre est "+nombreDesClasses+"\n");
+            this.currentPage++;
         }
        
     }
+    public void dansLesNomsDesClassesPage(int nombreDesClasses){
+
+        if(nombreDesClasses == 0){
+            //get the names of classes from the second page
+            List<String> classes = ((LesNomsDesClasses)cardPanels.getComponents()[1]).getLesNomDesClass();
+            String[] classesArray = new String[classes.size()];
+            classesArray = classes.toArray(classesArray);
+            //add the names of classes to the types used in Methodes and parameters
+            ((Methodes)cardPanels.getComponents()[3]).addToTypes(classesArray);
+            //pass to next page
+            this.currentPage++;
+            ((CardLayout)cardPanels.getLayout()).show(cardPanels, "4");
+        }
+        else{
+            ((LesNomsDesClasses)cardPanels.getComponents()[1]).getErrorsField().setText("Rester "+nombreDesClasses+" classes pour ajouter\n");
+
+        }
+
+    }
+
+
+    
  
     
     public static void main(String args[]) {
