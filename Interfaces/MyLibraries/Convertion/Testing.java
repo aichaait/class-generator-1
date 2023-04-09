243698be446d808cfd6c5255e189949380ce0181
bssayla <ouaicha47@gmail.com>
2023-04-09 14:30:08 +0000

    Done

--- Interfaces/MyLibraries/Convertion/Testing.java
+++ Interfaces/MyLibraries/Convertion/Testing.java
@@ -2,7 +2,7 @@ package MyLibraries.Convertion;
 
 public class Testing {
     public static void main(String[] args) {
-    String path  = "Interfaces/MyLibraries/Convertion/db_design.xml";
+    String path  = "MyLibraries/Convertion/db_design.xml";
     WriteXmlToJava wxtj = new WriteXmlToJava();
     wxtj.ConvertFromXml2Java(path);
 }
