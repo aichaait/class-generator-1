80f30709fdd7a86045421f8b3bd100db6f5d1533
bssayla <ouaicha47@gmail.com>
2023-04-25 15:08:28 +0100

    added

--- Interfaces/Panels/Apropos.java
+++ Interfaces/Panels/Apropos.java
@@ -1,25 +1,20 @@
 package Panels;
 
 import java.awt.Color;
-import java.awt.Font;
-
-import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 public class Apropos extends JPanel {
     JTextArea titreDePanel;
     public Apropos() {
         initComponents();
+        setBackground(new java.awt.Color(204, 217, 255));
     }
     private void initComponents() {
         titreDePanel = new JTextArea();
       // /JLabel apropos = new JLabel("welcom to UML2CODE generateur\nGenerate your code to cpp,java and sql ");
-        titreDePanel.setText("\t\tBienvenu a UML2CODE generateur\n");
+        titreDePanel.setText("\t\t            Apropos \n\n   Bienvenue dans notre générateur de classes Java à partir de bases de données XML.\nNotre générateur offre une solution efficace pour la création de classes Java,C++ et des \nrequêtes SQL à partir d'une base de données XML en utilisant le concept de mapping \nobjet-relationnel. Notre générateur est conçu pour vous permettre de spécifier les attributs,\nles méthodes et les relations entre les classes en utilisant une interface utilisateur intuitive.\n\nLes informations fournies par l'utilisateur sont ensuite stockées dans une base de données XML \nLe générateur prend ensuite en charge la conversion de cette base de données en classes Java,\nC++ ou des requêtes SQL.\n\nAvec notre générateur, vous pouvez facilement générer des classes qui correspondent à vos \nbesoins spécifiques en un temps record,et cela sans nécessiter une expertise en programmation. \nNous sommes convaincus que notre générateur vous aidera à accélérer le processus de \ndéveloppement et à améliorer la qualité de votre code.");
        // this.add(apropos);
        // titreDePanel.setHorizontalAlignment(SwingConstants.CENTER);
         titreDePanel.setFont(new java.awt.Font("Segoe UI Semibold", 0, 20));
